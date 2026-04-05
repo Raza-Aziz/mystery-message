6b410cfd700a00709e39c92de10368b85b9ca9c5
Raza Aziz <razaaziz9191@gmail.com>
2026-04-05 23:48:03 +0500

    feat: Implement API to get messages for a single user

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,32 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+
+  // if no session or user, then user ain't logged in
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 },
+    );
+  }
+
+  const userId = new mongoose.Types.ObjectId(user._id);
+
+  try {
+    // code here
+  } catch (error) {
+    console.error(error);
+  }
+}
